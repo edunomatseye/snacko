276e8bf9c9e11962d8e1e69f1cbf7958aaa41d0e
Edun Omatseye <edunomatseye@yahoo.com>
2023-09-05 11:10:16 +0100

    Added flexbox algorithm to app

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Fast refresh is really fast</Text>
+      <View style={styles.one}><Text>Open up App.js to start working on your app!</Text></View>
+      <View style={styles.two}><Text>Fast refresh is really fast</Text></View>
+      <View style={styles.three}><Text>Fast refresh</Text></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
+  one: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  two: {
+    flex: 2,
+    backgroundColor: 'yellow'
+  },
+  three: {
+    flex: 2,
+    backgroundColor: 'green'
+  }
 });
